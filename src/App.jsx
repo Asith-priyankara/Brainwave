4917cf9d3d2058dcc0d31a7a650ce018ed653e87
Priyankara WMA <asith.20@cse.mrt.ac.lk>
2024-08-25 01:47:43 +0530

    Implementation of Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
-
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./component/Button";
+import Header from "./component/Header";
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
     </>
   );
 }
